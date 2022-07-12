f369033fb35aea0038519362f9e9a5374d7e8904
Tyler Vo <tylervo@MacBook-Pro.local>
2022-07-12 02:01:07 -0400

    stuff

---
+++ src/Components/Education.js
@@ -0,0 +1,92 @@
+import React from 'react'
+
+function Education() {
+  return (
+    <section id="education" className="s-education target-section">
+        <div className="s-education__section">
+
+            <div className="row">
+                <div className="column">
+                    <h3 data-aos="fade-up">Education</h3>
+                </div>
+            </div>
+
+            <div className="row block-large-1-2 block-900-full work-positions">
+
+                <div className="column" data-aos="fade-up">
+                    <div className="position">
+                        <div className="position__header">
+                            <h6>
+                                <span className="position__co">UVA McIntire School of Commerce</span>
+                                <span className="position__pos">Bachelor of Science - BS, Commerce</span>
+                            </h6>
+                            <div className="position__timeframe">
+                                August 2022 - May 2024
+                            </div>
+                        </div>
+
+                        <p>
+                        Activities and societies: Creating Assests, Savings, and Hope (C.A.S.H) at Madison House, Virginia Case Club
+                        </p>
+                    </div>
+                </div> 
+                <div className="column" data-aos="fade-up">
+                    <div className="position">
+                        <div className="position__header">
+                            <h6>
+                                <span className="position__co">University of Virginia</span>
+                                <span className="position__pos">Bachelor of Arts - BA, Computer Science</span>
+                            </h6>
+                            <div className="position__timeframe">
+                                September 2020 - May 2024
+                            </div>
+                        </div>
+
+                        <p>
+                        Activities and societies: Vietnamese Student Association, IM Sports, VASST Ski Team, Greek Life (Social Chair)
+                        </p>
+                    </div>
+                </div> 
+                <div className="column" data-aos="fade-up">
+                    <div className="position">
+                        <div className="position__header">
+                            <h6>
+                                <span className="position__co">Mechville High School</span>
+                                <span className="position__pos">Advanced Studies Diploma</span>
+                            </h6>
+                            <div className="position__timeframe">
+                                September 2016 - June 2020
+                            </div>
+                        </div>
+
+                        <p>
+                        Activities and societies: Lacrosse (Co-Captain), Marching Band (Drum Major), National Honor Society, National Science Honor Society, French Honor Society, Tri-M Honor Society, Model UN, Key Club
+                        </p>
+                    </div>
+                </div> 
+                <div className="column" data-aos="fade-up">
+                    <div className="position">
+                        <div className="position__header">
+                            <h6>
+                                <span className="position__co">Summer Residential Governors School</span>
+                                <span className="position__pos">Math, Science, & Technology</span>
+                            </h6>
+                            <div className="position__timeframe">
+                                June 2019 - July 2019
+                            </div>
+                        </div>
+
+                        <p>
+                        Dirt Under the Fingernails Mathematics: Systems Which Evolve Over Time, Combinatorial Game Theory, Number Theory, Graph Theory, and Cellular Automaton.
+                        </p>
+                    </div>
+                </div> 
+
+            </div> 
+
+        </div> 
+    </section>
+  )
+}
+
+export default Education
